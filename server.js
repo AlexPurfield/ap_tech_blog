4c8bca5a1453c3ebad4f46dab07e9537366a5aaa
Alex Purfield <alexpurfield@gmail.com>
2023-10-12 16:25:30 -0600

    added file structure and some starter codde on config and db

---
+++ server.js
@@ -0,0 +1,5 @@
+const path = require ('path');
+const express = require ('express');
+const session = require ('session');
+const exphbs = require ('express-handlebars');
+const helpers = require ('./utils/helpers');
